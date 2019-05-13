18decb8bbf8084c7ad821a1576e81be76fef1ece
linct <314220356@qq.com>
2019-05-13 16:42:06 +0800

    28

---
+++ Easy/27.移除元素.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=javascript
+ *
+ * [27] 移除元素
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  for (let i = nums.length-1; i >=0; i--) {
+    if(nums[i]===val){
+      nums.splice(i,1)
+    }
+  }
+  return nums.length
+};
+
---
+++ Easy/28.实现str-str.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=javascript
+ *
+ * [28] 实现strStr()
+ */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  // 深究可至KMP算法
+    return haystack.indexOf(needle)
+};
+
