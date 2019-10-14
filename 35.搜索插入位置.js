b15809bfc05eb4fa5ade0aaa90e12c26a2d32eaa
linchaoting <linchaoting@duiba.com.cn>
2019-10-14 10:44:31 +0800

    35

--- 35.搜索插入位置.js
+++ 35.搜索插入位置.js
@@ -8,7 +8,15 @@
  * @param {number} target
  * @return {number}
  */
+
+//  可以改进为二分查找
 var searchInsert = function(nums, target) {
-    
+  for (let index = 0; index < nums.length; index++) {
+    const element = nums[index];
+    if (target===element || target<element) {
+      return index
+    }
+  }
+  return nums.length
 };
 
