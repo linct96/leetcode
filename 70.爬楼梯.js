48eeab1fa5fefa0fca6ff0652d953470ec4bcebf
linchaoting <linchaoting@duiba.com.cn>
2019-10-14 18:04:20 +0800

    38,70

---
+++ 70.爬楼梯.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=javascript
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode-cn.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (46.13%)
+ * Likes:    663
+ * Dislikes: 0
+ * Total Accepted:    89.2K
+ * Total Submissions: 190.3K
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * 注意：给定 n 是一个正整数。
+ * 
+ * 示例 1：
+ * 
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 
+ * 示例 2：
+ * 
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = function(n) {
+  if(n < 1){
+    return -1
+  }else if (n===1) {
+    return 1
+  }else if (n===2) {
+    return 2
+  }
+  //利用临时变量降低时间复杂度
+  let a = 1
+  let b = 2
+  let temp
+  while(n>2){
+    temp = b
+    b = a+b
+    a= temp
+    n--
+  }
+  return b
+};
+// @lc code=end
+
