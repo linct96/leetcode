8758173dd8b079a472ab0e91a27d39b82af16d9e
linct96 <314220356@qq.com>
2020-08-14 16:57:32 +0800

    update 7

--- 7.整数反转.js
+++ 7.整数反转.js
@@ -2,19 +2,62 @@
  * @lc app=leetcode.cn id=7 lang=javascript
  *
  * [7] 整数反转
+ *
+ * https://leetcode-cn.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (34.56%)
+ * Likes:    2103
+ * Dislikes: 0
+ * Total Accepted:    433.4K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '123'
+ *
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 123
+ * 输出: 321
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: -123
+ * 输出: -321
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: 120
+ * 输出: 21
+ * 
+ * 
+ * 注意:
+ * 
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回
+ * 0。
+ * 
  */
+
+// @lc code=start
 /**
+ * @description 转换为字符串再逆序
  * @param {number} x
  * @return {number}
  */
 var reverse = function(x) {
-  const MIN = -2147483648
-  const MAX = 2147483647
-  const reAbsNum = x<0?(Math.abs(x).toString().split('').reverse().join('')*-1):(Math.abs(x).toString().split('').reverse().join('')*1)
-  if(reAbsNum>MAX||reAbsNum<MIN){
-    return 0
+  let isNegative = false
+  const MAX = Math.pow(2,31)-1
+  const MIN = Math.pow(2,31)*-1
+  if (x<0) isNegative=true
+  let strArr = (Math.abs(x)+'').split('').reverse().join('')
+  if (isNegative) { 
+    strArr = strArr*-1
+    return MIN<=strArr?strArr:0
   }else{
-    return reAbsNum
+    return MAX>=strArr?strArr:0
   }
-}
+};
+// @lc code=end
 
